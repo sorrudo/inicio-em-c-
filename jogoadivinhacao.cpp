4c73db0b6fca877f8cd608def939e70101126931
sorrudo <radanguilherme@hotmail.com>
2022-01-04 16:04:56 -0300

    melhoramento do código

--- jogoadivinhacao.cpp
+++ jogoadivinhacao.cpp
@@ -3,24 +3,42 @@
  
  
  int main () {
-     cout << "****************************************" << endl;
-     cout << "* Seja Bem Vindo Ao Jogo De Adivinhação *" << endl;
-     cout << "****************************************" << endl;
+     cout << "**************************************" << endl;
+    cout << "* Bem-vindos ao jogo da adivinhação! *" << endl;
+    cout << "**************************************" << endl;
 
-     int numero_secreto = 42;
+    const int NUMERO_SECRETO = 42;
 
-     cout << "Digite Um Número ?" << endl;
-     int chute;
-     
-     cin >> chute;
-     cout <<"O valor do seu chute é: " << chute << endl;
+    bool nao_acertou = true;
+    int tentativas = 0;
 
-    bool acertou = chute == numero_secreto;
+    while(nao_acertou){
+        tentativas++;
+        int chute;
+        cout << "Tentativa " << tentativas << endl;
+        cout << "Qual seu chute? ";
+        cin >> chute;
 
-     if(acertou){
-         cout << "Parabéns você acertou o número !" << endl;
-     }else {
-         cout << "Infelizmente você errou o chute" << endl;
-     }
+        cout << "O valor do seu chute é: " << chute << endl;
+        bool acertou = chute == NUMERO_SECRETO;
+        bool maior = chute > NUMERO_SECRETO;
+
+        if (acertou)
+        {
+            cout << "Parabéns! Você acertou o número secreto!" << endl;
+            nao_acertou = false;
+        }
+        else if (maior)
+        {
+            cout << "Seu chute foi maior que o número secreto!" << endl;
+        }
+        else
+        {
+            cout << "Seu chute foi menor que o número secreto!" << endl;
+        }
+    }
+    cout << "Fim de jogo!" << endl;
+    cout << "Você acertou o número secreto em " << tentativas  << " tentativas" << endl;
+}
  }
  
